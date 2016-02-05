8a6cf9b9b6822fd0b394ac7905f22dd36c10e2f7
Stephanie Tong <tong.stephanie_mylinh@yahoo.com>
2016-02-05 15:47:45 -0800

    Created Buddylist Activity and Buddy Class

---
+++ app/src/main/java/fistbumpstudios/fistbump/Buddy.java
@@ -0,0 +1,10 @@
+package fistbumpstudios.fistbump;
+
+/**
+ * Created by Stephanie Tong on 2/5/2016.
+ *
+ * This file contains stuff for the Buddy class.
+ * Each buddy has a name, a unique ID (their MAC address), avatar, online status, etc.
+ */
+public class Buddy (String name, String id){
+}
