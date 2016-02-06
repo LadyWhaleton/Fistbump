089f2c16b0ed62dfa725201829e46ad748d6e255
Stephanie Tong <tong.stephanie_mylinh@yahoo.com>
2016-02-05 17:01:47 -0800

    Remade Buddylist Activity

--- app/src/main/java/fistbumpstudios/fistbump/Buddy.java
+++ app/src/main/java/fistbumpstudios/fistbump/Buddy.java
@@ -6,5 +6,28 @@ package fistbumpstudios.fistbump;
  * This file contains stuff for the Buddy class.
  * Each buddy has a name, a unique ID (their MAC address), avatar, online status, etc.
  */
-public class Buddy (String name, String id){
+public class Buddy {
+
+    private String name, id;
+
+    Buddy (String name, String id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public String getID
+    {
+        return this.id;
+    }
+
+    public void changeName(String newName)
+    {
+        this.name = newName;
+    }
 }
