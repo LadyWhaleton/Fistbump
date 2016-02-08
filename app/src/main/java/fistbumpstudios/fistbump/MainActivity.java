87a707378a9faf0c822d3d568d3d9c054c0dd845
ryosaito999 <ryocsaito@gmail.com>
2016-02-07 22:57:00 -0800

    fixed splash screen complier errors sorry stephanie

--- app/src/main/java/fistbumpstudios/fistbump/MainActivity.java
+++ app/src/main/java/fistbumpstudios/fistbump/MainActivity.java
@@ -166,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
             nfcAdapter.setBeamPushUris(
                     new Uri[]{Uri.fromFile(userInfo)}, this);
 
-            Intent intent = new Intent(this, setUserName.class);
+            Intent intent = new Intent(this, wait_beam.class);
             startActivity(intent);
             finish();
         }
