cf3f834c288663db1fe5b1396ed13ad940aefd54
ryosaito999 <ryocsaito@gmail.com>
2016-02-07 18:31:08 -0800

    fix nfc permission and  removed button

--- app/src/main/java/fistbumpstudios/fistbump/MainActivity.java
+++ app/src/main/java/fistbumpstudios/fistbump/MainActivity.java
@@ -45,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     }
 
 
