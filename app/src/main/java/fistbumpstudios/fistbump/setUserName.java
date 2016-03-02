681f76faee19dad2f8c10979e144bc8b21c63097
ryosaito999 <ryocsaito@gmail.com>
2016-03-01 16:47:01 -0800

    set profile pic works

--- app/src/main/java/fistbumpstudios/fistbump/setUserName.java
+++ app/src/main/java/fistbumpstudios/fistbump/setUserName.java
@@ -3,23 +3,31 @@ package fistbumpstudios.fistbump;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class setUserName extends AppCompatActivity {
-    File userfile;
+
+
     final public static String userFilename = "userInfo.txt";
+    private static int RESULT_LOAD_IMAGE = 1;
+    String picturePath = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,9 +48,9 @@ public class setUserName extends AppCompatActivity {
             FileOutputStream fos = openFileOutput(userFilename, Context.MODE_PRIVATE);
 
             //FileOutputStream fos = new FileOutputStream("user_info.txt");
-            username = username +'\n';
-            fos.write(username.getBytes());
-            fos.write(getMAC().getBytes());
+            fos.write((username + "\n").getBytes());
+            fos.write((getMAC() + "\n").getBytes());
+            fos.write(picturePath.getBytes());
             fos.close();
 
         } catch (FileNotFoundException e) {
@@ -51,7 +59,7 @@ public class setUserName extends AppCompatActivity {
             e.printStackTrace();
         }
     }
-    public void setUname (View view){
+    public void verifyFields(View view){
 
         EditText mEdit=(EditText)findViewById(R.id.username);
         String uname = mEdit.getText().toString();
@@ -61,11 +69,45 @@ public class setUserName extends AppCompatActivity {
                     Toast.LENGTH_SHORT).show();
             return;
         }
-        Toast.makeText(this, "Set Username to " + uname ,
-                Toast.LENGTH_SHORT).show();
+
+        if(picturePath.equals("")){
+            Toast.makeText(this, "Select a profile picture!" + uname,
+                    Toast.LENGTH_SHORT).show();
+            return;
+
+        }
         makeVerifyFile(uname);
         Intent intent = new Intent(this, tabbedMain.class);
         startActivity(intent);
         finish();
+    }
+
+    public void selectProfilePic(View view){
+        Intent i = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.profilePreview);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
     }
 }
