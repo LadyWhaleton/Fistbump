c8e7b538b4fc293c9a105902d1bc9ac55f61c3e6
ryosaito999 <ryocsaito@gmail.com>
2016-03-06 15:52:48 -0800

    removing comments

--- app/src/main/java/fistbumpstudios/fistbump/MediaGalleryTab.java
+++ app/src/main/java/fistbumpstudios/fistbump/MediaGalleryTab.java
@@ -3,7 +3,6 @@ package fistbumpstudios.fistbump;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -13,7 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -27,20 +25,9 @@ import java.util.Date;
 import java.util.List;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MediaGalleryTab.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MediaGalleryTab#newInstance} factory method to
- * create an instance of this fragment.
- * http://developer.android.com/guide/topics/media/mediaplayer.html
- */
 public class MediaGalleryTab extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     GridView galleryGridView;
     TextView emptyGridView;
@@ -53,8 +40,6 @@ public class MediaGalleryTab extends Fragment {
     private int defaultFileIcon = R.drawable.file_icon_generic_file;
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -62,31 +47,15 @@ public class MediaGalleryTab extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MediaGalleryTab.
-     */
     // TODO: Rename and change types and number of parameters
     public static MediaGalleryTab newInstance(String param1, String param2) {
         MediaGalleryTab fragment = new MediaGalleryTab();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -120,7 +89,6 @@ public class MediaGalleryTab extends Fragment {
         LoadMedia();
 
         // TODO: replace load media with LiveUpdate. Move LoadMedia to tabbedMain
-
         setListClickListener();
 
     }
@@ -172,16 +140,6 @@ public class MediaGalleryTab extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
