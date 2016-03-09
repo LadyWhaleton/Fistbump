f5a93431f1665a4e74421573c21f0d32beb0c739
Stephanie Tong <tong.stephanie_mylinh@yahoo.com>
2016-03-09 15:54:25 -0800

    Splash animation now has random text :D

--- app/src/main/java/fistbumpstudios/fistbump/SplashActivity.java
+++ app/src/main/java/fistbumpstudios/fistbump/SplashActivity.java
@@ -2,13 +2,28 @@ package fistbumpstudios.fistbump;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
 
 public class SplashActivity extends Activity {
 
     ImageView splash_iv;
+    String[] splashMsgs = {
+            "Readying consecutive normal punches..",
+            "ONEEE PAAWWWNNCCHHHHH!",
+            "Bro fist, dude!",
+            "Whales.",
+            "FALCON PUNCH!",
+            "Hello World.",
+            "Don't break your phones! :)"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +31,19 @@ public class SplashActivity extends Activity {
         setContentView(R.layout.splash_screen);
         ImageView img = (ImageView)findViewById(R.id.splash_iv);
         img.setBackgroundResource(R.drawable.anim);
+
+        int min = 0;
+        int max = splashMsgs.length;
+
+        Random r = new Random();
+        r.setSeed(System.currentTimeMillis());
+        int randomNum = r.nextInt(max - 0 + 1) + min;
+        String text = splashMsgs[randomNum];
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "BuxtonSketch.ttf");
+        TextView splashText = (TextView) findViewById(R.id.splash_tv);
+        splashText.setText(text);
+        splashText.setTypeface(font);
 
         // Get the background, which has been compiled to an AnimationDrawable object.
         AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
